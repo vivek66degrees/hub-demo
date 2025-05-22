54905230b7fa699daea27cf79b5f9f1ee0ef4d79
Vivek Chavhan <vivek.chavhan@66degrees.com>
2025-05-22 14:04:38 +0000

    .

---
+++ definitions/sources/source.js
@@ -0,0 +1,12 @@
+declare({
+database: "hub-interndev-env-20250516",
+schema: "hub_dna_bronze_dev",
+name: "Raw_act_log_plan_assoc"
+});
+
+
+declare({
+database: "hub-interndev-env-20250516",
+schema: "hub_dna_bronze_dev",
+name: "Raw_act_log"
+});
