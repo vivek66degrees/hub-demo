0a25d4c7553bd668017b9facefbf2ea83ba47b00
Vivek Chavhan <vivek.chavhan@66degrees.com>
2025-05-28 10:02:49 +0000

    .

--- includes/functions.js
+++ includes/functions.js
@@ -1,6 +1,6 @@
 function external_table(table, dataset, gcs_uri) {
     return `
-        CREATE OR REPLACE EXTERNAL TABLE ${dataset}.${table}
+        CREATE OR REPLACE EXTERNAL TABLE ${dataset}.ext_${table}
         OPTIONS (
         format = "PARQUET",
         uris = ["${gcs_uri}/*.parquet"]
